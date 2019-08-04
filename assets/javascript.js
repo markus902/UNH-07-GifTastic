33295a5c13f58ac22fa65ae7816e61bf8d672f96
Markus Markowich <markus902@gmx.de>
2019-08-03 21:23:27 -0400

    added pg rating, added error message when search field is empty

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,53 +1,80 @@
 let topics = ["Bratwurst", "Hulk", "Sky", "Trumpet", "Law and Order"];
 let key = "plDeGkLT5mfMoLkcJNrZi66kXLxQ02ce";
 let queryLink;
-newButton = $("<a>");
+let newButton = $("<a>");
+let adding = false;
 
 //Click events for buttons
 
-$(document).ready(function(){
+$(document).ready(function () {
 
     //Creating initial buttons
 
-    for(i = 0; i < topics.length; i++){
+    topics.forEach(function (topic) {
         newButton = $("<a>");
-        newButton.addClass("waves-effect waves-light btn").attr("value", topics[i]).text(topics[i]);
+        newButton.addClass("waves-effect waves-light btn").attr("value", topic).text(topic);
         $("#buttons").append(newButton);
         newButton = 0;
-    }; 
+    });
 
     // Creating new button
-    $("#add-btn").on("click", function(){
-        newButton = $("<a>");
-        let search = $("#search-field").val().trim();
-        newButton.addClass("waves-effect waves-light btn").attr("value", search).text(search);
-        $("#buttons").append(newButton);
-    })
+    $("#add-btn").on("click", function (event) {
+        if ($("#search-field").val() == "") {
+            $("#search-field").attr("placeholder", "Please enter a category");
+        } else {
+            newButton = $("<a>");
+            let search = $("#search-field").val().trim();
+            newButton.addClass("waves-effect waves-light btn")
+                .attr("value", search)
+                .text(search);
+            $("#buttons").append(newButton);
+        }
+    });
+
 
     // Click event on button
-    $(".btn").on("click", function(){
+    $(document).on("click", ".btn", function () {
         console.log(this);
         let name = $(this).attr("value");
         console.log(name);
-    
+
         let queryLink = `https://api.giphy.com/v1/gifs/search?api_key=${key}&q=${name}&limit=10`;
 
         $.ajax({
-            url: queryLink,
-            method: "GET",
-        })
+                url: queryLink,
+                method: "GET",
+            })
 
-        .then(function(gifs){
+            .then(function (gifs) {
+                console.log(gifs.data);
 
-            for(i = 0; i < gifs.data.length; i++){
-                console.log(gifs.data[0].images.downsized.url);
-                let newImg = $("<img>");
-                let gifUrl = gifs.data[i].images.downsized.url;
-                newImg.attr("src", gifUrl).addClass(`gif${i}`);
-                $("#output").prepend(newImg);
-                newImg = 0;
-            }   
-        }) 
+                $("#output").empty();
+                for (i = 0; i < gifs.data.length; i++) {
+                    console.log(gifs.data[0].images.fixed_height_still.url);
+                    let newImgStill = $("<br><img>")
+                        .attr("src", gifs.data[i].images.fixed_height_still.url);
+                    let newImgActive = $("<img>")
+                        .attr("src", gifs.data[i].images.fixed_height.url)
+                        .css("display", "none");
+                    let newRating = $("<span>")
+                        .css("float", "none")
+                        .text("Rating: " + gifs.data[i].rating);
+                    let newDiv = $("<div>")
+                        .css("width", "fit-content")
+                        .css("float", "left")
+                        .css("text-align", "center");
+
+                    newDiv.append(newRating);
+                    newDiv.append(newImgStill);
+                    newDiv.append(newImgActive);
+
+                    $("#output").prepend(newDiv);
+                }
+            });
     });
 
+    $(document).on("click", "img", function () {
+        $(this).siblings("img").show();
+        $(this).hide();
+    });
 });
